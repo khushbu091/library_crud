632bf153ee8408f0d4cb0d1195ef4541e8b4518a
khushi <sourabhpatel63679@gmail.com>
2024-05-17 16:55:43 +0530

    updated

--- project/app/urls.py
+++ project/app/urls.py
@@ -13,6 +13,11 @@ urlpatterns = [
     path('dashbord/', dashbord, name='dashbord'),
     path('query/',query,name='query'),
     path('showdata/',showdata,name='showdata'),
+    path('delete/<int:pk>/<ml>',delete,name='delete'),
+    path('edit/<int:pk>/<ml>',edit,name='edit'),
+
+
+
 
     # path('showdata/',showdata,name='showdata')
     # path('logout',logout,name='logout')
--- project/app/views.py
+++ project/app/views.py
@@ -17,21 +17,21 @@ def login(request):
     return render(request, 'login.html')
 def registerdata(request):
     name=request.POST.get('name')
-    email=request.POST.get('email')
+    emails=request.POST.get('email')
     contact=request.POST.get('contact')
     password=request.POST.get('password')
     cpassword=request.POST.get('cpassword')
    
-    data=Student.objects.filter(Email=email)
+    data=Student.objects.filter(Email=emails)
     print(data)
 
     if data:
-        msg="Email already exist" 
+        msg="Emails already exist" 
         return render(request,'register.html',{'key':msg})
     else:
         if password==cpassword:
             Student.objects.create(Name=name,
-                                     Email=email,
+                                     Email=emails,
                                      Contact=contact,
                                      Password=password,
                                     )
@@ -42,11 +42,11 @@ def registerdata(request):
             msg="password and Confirm Password not matched"
             return render(request,'register.html',{'key':msg})
 def logindata(request):
-    email=request.POST.get('email')
+    emails=request.POST.get('email')
     password=request.POST.get('password')
-    user=Student.objects.filter(Email=email)
+    user=Student.objects.filter(Email=emails)
     if user:
-        data=Student.objects.get(Email=email)
+        data=Student.objects.get(Email=emails)
         pss=data.Password
         if pss==password:
             context={
@@ -57,10 +57,10 @@ def logindata(request):
             }
             return render(request,'dashbord.html',{'context':context})
         else:
-            msg='Enter valid email'
+            msg='Enter valid emails'
             return render(request,'login.html',{'key':msg})
     else:
-        msg="enter valid email"
+        msg="enter valid emails"
         return render (request,'login.html',{'key':msg})
         
 
@@ -75,42 +75,66 @@ def query(request):
     print(request.POST)
     tittle=request.POST.get('tittle')
     dec=request.POST.get('dec')
-    email=request.POST.get('email')
+    emails=request.POST.get('email')
     password=request.POST.get('password')
 
-    Query.objects.create(Tittle=tittle,Dec=dec,Email=email,Password=password)
-    print("sumit",email)
-    data = Query.objects.filter(Email=email)
-    ans=data.values()
-    print("khushi",ans)
-    # tittle=data.Tittle
-    # dec=data.Dec
-    # email=data.Email
-    # password=data.Password
-
-    user={
-        'titlle':tittle,
-        'dec':dec,
-        'email':email,
-        'password':password,
-
-    }  
-    all_data=Query.objects.filter(Email=email)
-    return render(request,'dashbord.html',{'key':all_data,'user':user})
+    Query.objects.create(Tittle=tittle,Dec=dec,Email=emails,Password=password)
+    user1 = Student.objects.get(Email=emails)
+    context={
+                'nm':user1.Name,
+                'em':user1.Email,
+                'pass':user1.Password,
+                'logout':'logout'
+            } 
+    all_data=Query.objects.filter(Email=emails)
+    return render(request,'dashbord.html',{'key':all_data,'context':context})
 
 def showdata(request):
-    #select * from tablename
-    #for fetching all the data of the table
-    data = Query.objects.get(Email=email)
-    tittle = data.Tittle
-    dec = data.Dec
-    email = data.Email
-   
-    user={
-        'tittle':tittle,
-        'dec':dec,
-        'email':email,
-        
-    }
-    all_data=Query.objects.filter(Email=email)
-    return render(request,'dashbord.html',{'key':all_data,'user':user})
+    print(request.POST)
+    emails=request.POST.get('email')
+
+    datas=Query.objects.filter(Email=emails)
+    if datas:
+       user1=Student.objects.get(Email=emails)
+       context={
+                'nm':user1.Name,
+                'em':user1.Email,
+                'pass':user1.Password,
+                'logout':'logout'
+            } 
+       all_data=Query.objects.filter(Email=emails)
+       return render(request,'dashbord.html',{'key1':all_data,'context':context})
+
+def delete(request,pk,ml):
+    sdel=Query.objects.get(id=pk)
+    sdel.delete()
+    eml=Query.objects.filter(Email=ml)
+    msg='data delete'
+    datas=Query.objects.filter(Email=ml)
+    if datas:
+        user1=Student.objects.get(Email=ml)
+        context={
+                'nm':user1.Name,
+                'em':user1.Email,
+                'pass':user1.Password,
+                'logout':'logout'
+            } 
+    all_data=Query.objects.filter(Email=ml)
+    return render(request,'dashbord.html',{'key1':all_data,'context':context})
+
+def edit(request,pk,ml):
+    sedt=Query.objects.get(id=pk)
+    ml=Query.objects.filter(Email=ml)
+    msg='edit data'
+    datas=Query.objects.filter(Email=ml)
+    if datas:
+        user1=Student.objects.get(Email=ml)
+        context={
+                'nm':user1.Name,
+                'em':user1.Email,
+                'pass':user1.Password,
+                'logout':'logout'
+            } 
+    all_data=Query.objects.filter(Email=ml)
+    return render(request,'dashbord.html',{'key1':all_data,'context':context})
+
